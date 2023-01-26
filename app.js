fdd0e00117739146daa11155bdffb2a12014ff8c
SURYA HADI PRASTYA <hadisurya295@gmail.com>
2023-01-26 21:40:54 +0700

    mdof

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const moment = require("moment/moment");
 const { default: axios } = require("axios");
 const { Platform } = require("selenium-webdriver/lib/capabilities");
 const { platform } = require("process");
+const { rimrafSync } = require("rimraf/dist/cjs/src");
 let watchingTimeLimit = 5 * 60 * 1000;
 
 function convert(file) {
@@ -42,11 +43,23 @@ function convert(file) {
 }
 
 class DriveOtomatis {
+    async removeDrive() {
+        if (this.driver == null) {
+            return;
+        }
+        await this.driver.quit();
+        console.log('s', this.userdir)
+        if (platform.includes("win")) {
+            rimraf.windowsSync(this.userdir);
+        } else {
+            rimraf.moveRemoveSync(this.userdir)
+        }
+    }
     constructor({ url, username, password }) {
         try {
-            var temp = "/tmp/chrome/"
+            var temp = "/tmp/chrome_profiles/" + getRndInteger(2000, 13000);
             if (platform.includes("win")) {
-                temp = "C:\\temp\\"
+                temp = "C:\\temp\\" + getRndInteger(2000, 13000);
             }
             console.log('chormepath', { path: chromePaths.chrome, temp })
             var chromeCapabilities = Capabilities.chrome();
@@ -66,9 +79,9 @@ class DriveOtomatis {
             chromeOptions.addArguments('autodetect=false');
             chromeOptions.addArguments('log-level=3');
             chromeOptions.addArguments('disable-logging');
-            let x = temp + getRndInteger(2000, 13000);
+            let x = temp
             chromeOptions.addArguments(`profile-directory=${x}`);
-            // chromeOptions.addArguments(`user-data-dir=${x}`);
+            chromeOptions.addArguments(`user-data-dir=${x}`);
             // const randomOS = [Platform.MAC, Platform.LINUX, Platform.WINDOWS];
             // chromeOptions.setPlatform(randomOS[Math.floor(Math.random() * randomOS.length)])
             // const deviceName = ['iPhone SE', 'iPhone XR', 'iPhone 12 Pro', 'iPhone X',    'PIXEL 5'];
@@ -431,14 +444,7 @@ class DriveOtomatis {
             return
         }
     }
-    async removeDrive() {
-        if (this.driver == null) {
-            return;
-        }
-        await this.driver.quit();
-        console.log('s', this.userdir)
-        // rimraf.windowsSync(this.userdir);
-    }
+
 }
 // const urlList = [
 //     { keyword: "digitopupstore.com", url: 'https://digitopupstore.com/', type: "google" },
