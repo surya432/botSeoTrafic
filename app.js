a83e418d9b6ba1cb5e395c25bef372b1b48ba748
SURYA HADI PRASTYA <hadisurya295@gmail.com>
2023-01-26 22:05:41 +0700

    modif

--- app.js
+++ app.js
@@ -119,16 +119,16 @@ class DriveOtomatis {
 
             var driver = new Builder();
             const listBrowser = [
-                Browser.CHROME,
-                Browser.CHROME,
-                Browser.CHROME,
-                Browser.FIREFOX,
-                Browser.CHROME,
-                Browser.CHROME,
-                Browser.CHROME,
-                Browser.EDGE,
-                Browser.CHROME,
-                Browser.CHROME,
+                // Browser.CHROME,
+                // Browser.CHROME,
+                // Browser.CHROME,
+                // Browser.FIREFOX,
+                // Browser.CHROME,
+                // Browser.CHROME,
+                // Browser.CHROME,
+                // Browser.EDGE,
+                // Browser.CHROME,
+                // Browser.CHROME,
                 Browser.CHROME,
             ];
             const browserSelected = listBrowser[Math.floor(Math.random() * listBrowser.length)]
